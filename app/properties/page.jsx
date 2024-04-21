aced29f0d7da94aed996de96d218881a558fc9f6
loloDawit <dawitabera64@gmail.com>
2024-04-21 12:58:30 -0700

    Refactor PropertiesPage component to display properties from properties.json

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import properties from '@/properties.json'
+import CardView from '@/components/CardView'
 
 const PropertiesPage = () => {
-    return <div>PropertiesPage</div>
+    return (
+        <section class="px-4 py-6">
+            <div class="container-xl lg:container m-auto px-4 py-6">
+                {properties.length === 0 ? (
+                    <p>No properties found</p>
+                ) : (
+                    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map((property) => (
+                            <CardView key={property._id} property={property} />
+                        ))}
+                    </div>
+                )}
+            </div>
+        </section>
+    )
 }
 
 export default PropertiesPage
