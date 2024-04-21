774614081aa9804744e555f45522e1a0e2f61fa7
loloDawit <dawitabera64@gmail.com>
2024-04-21 12:56:27 -0700

    Add LoadingPage component to display a loading spinner

---
+++ app/loading.jsx
@@ -0,0 +1,20 @@
+'use client';
+import MoonLoader from 'react-spinners/MoonLoader';
+
+const override = {
+  display: 'block',
+  margin: '100px auto',
+};
+
+const LoadingPage = ({ loading }) => {
+  return (
+    <MoonLoader
+      color='#3b82f6'
+      loading={loading}
+      cssOverride={override}
+      size={100}
+      aria-label='Loading Spinner'
+    />
+  );
+};
+export default LoadingPage;
