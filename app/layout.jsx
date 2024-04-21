60a0ae7701b9970f9c588c118aebe076f4e8fb46
loloDawit <dawitabera64@gmail.com>
2024-04-21 12:58:13 -0700

    Add Footer component to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@/assets/styles/globals.css'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export const metadata = {
     title: 'Find your next home',
@@ -14,6 +15,7 @@ const MainLayout = ({ children }) => {
             <body>
                 <Navbar />
                 <main>{children}</main>
+                <Footer />
             </body>
         </html>
     )
