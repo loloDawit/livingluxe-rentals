8e9390a0a641e6952260b7a204575d32199fd485
loloDawit <dawitabera64@gmail.com>
2024-04-21 12:57:05 -0700

    Add Footer component to display copyright information

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    const currentYear = new Date().getFullYear()
+
+    return (
+        <footer className="bg-gray-200 py-4 fixed bottom-0 left-0 w-full">
+            <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
+                <div>
+                    <p className="text-sm text-gray-500 mt-2 md:mt-0">
+                        &copy; {currentYear} PureLifeHomes. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
