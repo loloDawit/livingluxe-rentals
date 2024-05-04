83dfda7285c086e572be96ed7add31506a0024c1
loloDawit <dawitabera64@gmail.com>
2024-05-04 15:05:19 -0700

    refactor: Remove password hashing logic from User model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 import { Schema, model, models } from 'mongoose'
-import bcrypt from 'bcrypt'
 
 const UserSchema = new Schema(
     {
@@ -15,11 +14,6 @@ const UserSchema = new Schema(
             type: String,
             required: true,
             trim: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            minlength: 6,
         },
         image: String,
         bookmarks: [
@@ -37,14 +31,6 @@ const UserSchema = new Schema(
 // Indexes
 UserSchema.index({ email: 1, username: 1 })
 
-// Middleware
-UserSchema.pre('save', async function (next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10) // Hash password before saving
-    }
-    next()
-})
-
 // Static methods
 UserSchema.statics.findByEmail = function (email) {
     return this.findOne({ email })
@@ -52,11 +38,6 @@ UserSchema.statics.findByEmail = function (email) {
 
 UserSchema.statics.findByUsername = function (username) {
     return this.findOne({ username })
-}
-
-// Instance methods
-UserSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password) // Compare hashed password
 }
 
 const User = models.User || model('User', UserSchema)
