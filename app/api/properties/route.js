c0854c9f2f718478fb9f1eca92f04fd7deb7cc92
loloDawit <dawitabera64@gmail.com>
2024-05-04 15:07:41 -0700

    refactor: Add image upload functionality to POST /api/properties route

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,9 @@
 import initializeDatabase from '@/config/db.config'
 import Property from '@/models/Property'
+import { getSessionUser } from '@/utils/getSessionUser'
+import { extractFormData } from '@/utils/helpers/common'
+import cloudinary from '@/config/cloudinary'
+
 // GET /api/properties
 export const GET = async (req, res) => {
     try {
@@ -8,5 +12,55 @@ export const GET = async (req, res) => {
         return new Response(JSON.stringify(properties), { status: 200 })
     } catch (error) {
         return new Response('something went wring', { status: 500 })
+    }
+}
+
+// POST /api/properties
+export const POST = async (req, res) => {
+    try {
+        const propertyData = await extractFormData(req)
+        console.log(propertyData.images)
+
+        await initializeDatabase()
+        const session = await getSessionUser()
+        console.log(session)
+
+        if (!session) {
+            return new Response('Unauthorized', { status: 401 })
+        }
+
+        const imageUploadPromises = []
+
+        for (const image of propertyData.images) {
+            console.log(image)
+            const imageBuffer = await image.arrayBuffer()
+            const imageArray = Array.from(new Uint8Array(imageBuffer))
+            const imageData = Buffer.from(imageArray).toString('base64')
+            const uploadedImage = await cloudinary.uploader.upload(
+                `data:image/jpeg;base64,${imageData}`,
+                { folder: 'airbnb-clone' }
+            )
+            imageUploadPromises.push(uploadedImage.secure_url)
+        }
+        // Wait for all images to upload
+        const uploadedImages = await Promise.all(imageUploadPromises)
+        // Add uploaded images to the propertyData object
+        propertyData.images = uploadedImages
+
+        const userId = session.userId
+        const property = new Property({ ...propertyData, owner: userId })
+        await property.save()
+
+        console.log(property)
+
+        return Response.redirect(
+            `${process.env.NEXTAUTH_URL}/properties/${property._id}`
+        )
+        // return new Response(JSON.stringify({ message: 'success' }), {
+        //     status: 201,
+        // })
+    } catch (error) {
+        console.log(error)
+        return new Response('something went wrong', { status: 500 })
     }
 }
