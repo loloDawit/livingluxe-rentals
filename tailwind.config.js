e3cd9b6a3ebc44d8a74873f5e18cbcf1a65f4190
loloDawit <dawitabera64@gmail.com>
2024-05-04 15:02:45 -0700

    refactor: Update tailwind.config.js with font and grid layout changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-              sans: ['Poppins', 'sans-serif'],
+                sans: ['Poppins', 'sans-serif'],
             },
             gridTemplateColumns: {
-              '70/30': '70% 28%',
+                '70/30': '70% 28%',
             },
-          },
+        },
     },
     plugins: [],
 }
