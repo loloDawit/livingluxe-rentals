c56d944232275dc3d994093214bf7aa6fd4e76c7
loloDawit <dawitabera64@gmail.com>
2024-05-18 23:23:18 -0700

    config update and add dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Poppins', 'sans-serif'],
+                sans: ['CostarBrownRegular', 'sans-serif'],
             },
             gridTemplateColumns: {
                 '70/30': '70% 28%',
