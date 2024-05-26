5e5633fd53f3f5df3fd5172f99f5611c6be8d2bc
loloDawit <dawitabera64@gmail.com>
2024-05-25 23:02:22 -0700

    refactor(metadata): Update title, description, and keywords for better SEO

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,8 +8,10 @@ import 'react-toastify/dist/ReactToastify.css'
 
 export const metadata = {
     title: 'Discover Your Next Dream Home',
-    description: 'Explore a wide range of rental properties to find your perfect home. Whether you are looking for apartments, houses, or condos, we have listings that match your needs and budget.',
-    keywords: 'rental properties, find your next home, dream home, apartments for rent, houses for rent, condos for rent, rental listings, property search',
+    description:
+        'Explore a wide range of rental properties to find your perfect home. Whether you are looking for apartments, houses, or condos, we have listings that match your needs and budget.',
+    keywords:
+        'rental properties, find your next home, dream home, apartments for rent, houses for rent, condos for rent, rental listings, property search',
 }
 
 const MainLayout = ({ children }) => {
