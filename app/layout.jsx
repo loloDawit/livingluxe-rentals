985a9a878d35432283db2b5a8613df840eb2850f
loloDawit <dawitabera64@gmail.com>
2024-05-25 21:46:19 -0700

    refactor(metadata): enhance title, description, and keywords for better SEO

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,9 +7,9 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 export const metadata = {
-    title: 'Find your next home',
-    description: 'Find your dream home',
-    keywords: 'Find rental, find your next home',
+    title: 'Discover Your Next Dream Home',
+    description: 'Explore a wide range of rental properties to find your perfect home. Whether you are looking for apartments, houses, or condos, we have listings that match your needs and budget.',
+    keywords: 'rental properties, find your next home, dream home, apartments for rent, houses for rent, condos for rent, rental listings, property search',
 }
 
 const MainLayout = ({ children }) => {
