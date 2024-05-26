db2bcf9aa21743fabc31e87b27558de3afb5d4c0
loloDawit <dawitabera64@gmail.com>
2024-05-26 13:46:24 -0700

    refactor: Update InfoBoxes component to use dynamic data

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -2,36 +2,45 @@ import React from 'react'
 import InfoBox from './InfoBox'
 
 const InfoBoxes = () => {
+    const infoBoxData = [
+        {
+            heading: 'For Renters',
+            buttonInfo: {
+                text: 'Browse Properties',
+                link: '/properties',
+                backgroundColor: 'bg-black',
+            },
+            content:
+                'Find your dream rental property. Bookmark your favorite listings and contact the owners directly.',
+        },
+        {
+            heading: 'For Property Owners',
+            backgroundColor: 'bg-blue-100',
+            buttonInfo: {
+                text: 'Add Property',
+                link: '/properties/add',
+                backgroundColor: 'bg-blue-500',
+            },
+            content:
+                'Rent your properties as Airbnb or long-term leases and connect with tenants.',
+        },
+    ]
+
     return (
         <div>
-            {/* <!-- Renters and Owners --> */}
             <section>
                 <div className="container-xl lg:container m-auto">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-                        <InfoBox
-                            heading="For Renters"
-                            buttonInfo={{
-                                text: 'Browse Properties',
-                                link: '/properties',
-                                backgroundColor: 'bg-black',
-                            }}
-                        >
-                            Find your dream rental property. Bookmark properties
-                            and contact owners.
-                        </InfoBox>
-
-                        <InfoBox
-                            heading="For Property Owners"
-                            backgroundColor="bg-blue-100"
-                            buttonInfo={{
-                                text: 'Add Property',
-                                link: '/properties/add',
-                                backgroundColor: 'bg-blue-500',
-                            }}
-                        >
-                            List your properties and reach potential tenants.
-                            Rent as an airbnb or long term.
-                        </InfoBox>
+                        {infoBoxData.map((info, index) => (
+                            <InfoBox
+                                key={index}
+                                heading={info.heading}
+                                backgroundColor={info.backgroundColor}
+                                buttonInfo={info.buttonInfo}
+                            >
+                                {info.content}
+                            </InfoBox>
+                        ))}
                     </div>
                 </div>
             </section>
