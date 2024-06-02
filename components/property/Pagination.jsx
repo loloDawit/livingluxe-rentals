0e3068005d5ab37cd3c0d94e24fe320cc4e46f05
loloDawit <dawitabera64@gmail.com>
2024-06-02 00:25:04 -0700

    feat: Add Pagination component for navigating through pages

---
+++ components/property/Pagination.jsx
@@ -0,0 +1,34 @@
+const Pagination = ({ page, totalPages, onPageChange }) => {
+    const handlePageChange = (newPage) => {
+        if (newPage >= 1 && newPage <= totalPages) {
+            onPageChange(newPage)
+        }
+    }
+
+    if (totalPages <= 1) {
+        return null;
+    }
+
+    return (
+        <section className="container mx-auto flex justify-center items-center my-8">
+            <button
+                className="mr-2 px-2 py-1 border border-gray-300 rounded"
+                disabled={page === 1}
+                onClick={() => handlePageChange(page - 1)}
+            >
+                Previous
+            </button>
+            <span className="mx-2">
+                Page {page} of {totalPages}
+            </span>
+            <button
+                className="ml-2 px-2 py-1 border border-gray-300 rounded"
+                disabled={page === totalPages}
+                onClick={() => handlePageChange(page + 1)}
+            >
+                Next
+            </button>
+        </section>
+    )
+}
+export default Pagination
