18cd9ce4d9177dcf0738241bda8fb445278a5079
loloDawit <dawitabera64@gmail.com>
2024-06-02 00:24:31 -0700

    refactor: Update PropertiesPage to use Properties component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import CardView from '@/components/CardView'
-import { fetchProperties } from '@/utils/api.requests'
+
+import Properties from '@/components/property/Properties'
 
 const PropertiesPage = async () => {
-    const properties = await fetchProperties()
-    return (
-        <section className="px-4 py-6">
-            <div className="container-xl lg:container m-auto px-4 py-6">
-                {properties.length === 0 ? (
-                    <p>No properties found</p>
-                ) : (
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {properties.map((property) => (
-                            <CardView key={property._id} property={property} />
-                        ))}
-                    </div>
-                )}
-            </div>
-        </section>
-    )
+    return <Properties />
 }
 
 export default PropertiesPage
