8d33daa8ff6cc40735347d0f85f1bbb7aeddb70a
loloDawit <dawitabera64@gmail.com>
2024-06-02 16:04:14 -0700

    refactor: Update EditForm import to use correct casing

--- app/properties/[id]/edit/page.jsx
+++ app/properties/[id]/edit/page.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import EditForm from '@/components/property/Editform'
+import Editform from '@/components/property/Editform';
 
 const UpdateProperty = () => {
     return (
         <section className="bg-blue-50">
             <div className="container m-auto max-w-2xl py-24">
                 <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-                    <EditForm />
+                    <Editform />
                 </div>
             </div>
         </section>
