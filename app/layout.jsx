39c918439e5da50c56f82b62e9e65477bc6fbb81
loloDawit <dawitabera64@gmail.com>
2024-06-01 23:32:18 -0700

    refactor: Add GlobalProvider to layout component for global state management

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import Footer from '@/components/Footer'
 import AuthProvider from '@/components/AuthProvider'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { GlobalProvider } from '@/context/GlobalContext'
 
 export const metadata = {
     title: 'Discover Your Next Dream Home',
@@ -16,16 +17,18 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
     return (
-        <AuthProvider>
-            <html lang="en">
-                <body>
-                    <Navbar />
-                    <main>{children}</main>
-                    <Footer />
-                    <ToastContainer />
-                </body>
-            </html>
-        </AuthProvider>
+        <GlobalProvider>
+            <AuthProvider>
+                <html lang="en">
+                    <body>
+                        <Navbar />
+                        <main>{children}</main>
+                        <Footer />
+                        <ToastContainer />
+                    </body>
+                </html>
+            </AuthProvider>
+        </GlobalProvider>
     )
 }
 
